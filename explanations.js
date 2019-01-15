a5e3acf920d160aca0ee8f7d63af0ffb8db8dba4
Shiyue Xu <lapulapu133@gmail.com>
2019-01-15 21:38:35 +0800

    Linked up css and js

---
+++ explanations.js
@@ -0,0 +1,50 @@
+// Variables
+var fabColor = 'red'; // string
+var myFavColors = ['blue', 'red', 'green']; //array
+var numOfFavColors = 3; // num
+var hasGotFavColors = TRUE; //Boolean
+var Amore = {
+	firstName: 'O',
+	lastName: ':(',
+	why: 'oh no',
+	isAsshole: TRUE
+};
+
+// Functions
+var whatIsMyFavColor = function () {
+	return 'blue';
+} // anything after return does not run
+
+var doSomething = function (){
+	console.log("doSomething!!!")
+
+} // does stuff but returns undefined 
+
+var area = function (width, height){
+	return width * height
+} // here you need to enter parameters 
+
+var fullname = function (firstName, secondName) {
+	return firstName + ' ' + secondName
+}
+
+// IF statements 
+
+if('male' == 'male'){
+	alert('male');
+}
+
+var name1 = 'Latte';
+var name2 = 'Espresso';
+
+if (name1 == 'Latte'){
+	alert('True!!!');
+} else if (name2 == 'Espresso'){
+	alert('True also!!!');
+} else{
+	alert('HUHUHUHU');
+}
+
+var name1 == 'latte'
+
+
